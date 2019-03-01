51acee0d8d5bff3f9b8dcf3900db4de9c45e4570
Denis Budyak <budden73@gmail.com>
2019-03-01 22:59:19 +0300

    refactoring

--- main.go
+++ main.go
@@ -20,10 +20,12 @@ import (
 
 func main() {
 	url := "postgresql://localhost:5432"
-	db, err := sqlx.Open("postgres", url)
+
+	db, err, dbCloser := openDb(url)	
 	if err != nil { 
 		log.Printf("Unable to connect to Postgresql, error is %#v", err)
 		os.Exit(1) }
+	defer dbCloser()
 	m := map[string]interface{}{"name" : `",sql 'inject?`}
 	res, err1 := db.NamedExec(`insert into users values (:name)`,
 		m)
@@ -32,24 +34,64 @@ func main() {
 		switch e := interface{}(err1).(type)	{
 		case *pq.Error:
 			if e.Code == "23505" {
-				fmt.Printf("Duplicate key in %s",e.Constraint)
+				fmt.Printf("Duplicate key in %s",e.Constraint);
 			} else {
 				fmt.Printf("Error inserting: %#v\n",err1) }
 		default: 
 			fmt.Printf("Error insertiing: %#v\n",err1) }
 	} else {
 		fmt.Printf("Inserted %#v\n",res)	}
-	res1, err2 := db.Query(`select current_timestamp + interval '1' day`)
-	if err2 != nil {
-		fmt.Printf("Wow!"); os.Exit(1)	}
-	if !res1.Next() {
-		fmt.Printf("No rows here. Why?"); os.Exit(1) }
-	var magic time.Time
-	res1.Scan(&magic)
-	fmt.Printf("Expiry at %s\n", magic.Format("2006-01-02 15:04 -0700"));
+	genExpiryDate(db)
 
-	err = db.Close()
-	if err != nil {
-		log.Printf("Error closing db")
-		os.Exit(1) }
+	playWithPanic()
+	genNonce()
 	return }
+
+ func openDb(url string) (db *sqlx.DB, err error, closer func()) {
+		db, err = sqlx.Open("postgres", url)
+		closer = func() {
+			err := db.Close()
+			if err != nil {
+				fmt.Printf("Error closing db: %v\n",err)
+				// Not exiting because this function is called from the defer
+			}}
+		return }
+
+	func playWithPanic() {
+		unwind := func() {
+			if r := recover(); r != nil {
+				fmt.Printf("recover %#v\n",r)
+				//panic(r)
+			}
+		}
+		defer unwind()
+		panic("It's a panic")	
+	}
+
+
+	func genExpiryDate(db *sqlx.DB) {
+		res1, err2 := db.Query(`select current_timestamp + interval '1' day`)
+		if err2 != nil {
+			fmt.Printf("Wow!"); os.Exit(1)	}
+		if !res1.Next() {
+			fmt.Printf("No rows here. Why?"); os.Exit(1) }
+		var magic time.Time
+		res1.Scan(&magic)
+		fmt.Printf("Expiry at %s\n", magic.Format("2006-01-02 15:04 -0700"))	}
+
+/* def gen_nonce(length):
+   """ Generates a random string of bytes, base64 encoded """
+   if length < 1:
+      return ''
+   string=base64.b64encode(os.urandom(length),altchars=b'-_')
+   b64len=4*floor(length,3)
+   if length%3 == 1:
+      b64len+=2
+   elif length%3 == 2:
+      b64len+=3
+   return string[0:b64len].decode() */
+
+
+func genNonce()	{
+
+}
