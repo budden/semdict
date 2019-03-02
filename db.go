0b934b2fe52030050045d38063f41db36726aadc
Denis Budyak <budden73@gmail.com>
2019-03-03 00:09:04 +0300

    Added hashing and salting of the password

---
+++ db.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt";	"os"; "time"
+//	neturl "net/url"
+//	"reflect"
+//	"regexp"
+//	"strings"
+//	"time"
+ "database/sql"
+	"github.com/jmoiron/sqlx"
+	"github.com/lib/pq")
+
+func playWithDb() {
+ url := "postgresql://localhost:5432"
+
+	db, err, dbCloser := openDb(url)	
+	if err != nil { 
+		logicalPanic(fmt.Sprintf("Unable to connect to Postgresql, error is %#v", err)) }
+	defer dbCloser()
+ var rows *sql.Rows 
+	rows, err = db.Query(`drop table if exists budden_a`)
+	if err != nil { logicalPanic(fmt.Sprintf("Unable to drop table budden_a, original error: %#v", err))	}
+	rows.Close()
+	rows, err = db.Query(`create table budden_a (name text)`)
+	m := map[string]interface{}{"name" : `",sql 'inject?`}
+	for i := 0; i < 2; i++ {
+		var res sql.Result
+		res, err1 := db.NamedExec(`insert into users values (:name)`,
+			m)
+	//xt := reflect.TypeOf(err1).Kind()
+		if err1 != nil {
+			switch e := interface{}(err1).(type)	{
+			case *pq.Error:
+				if e.Code == "23505" {
+					fmt.Printf("Duplicate key in %s",e.Constraint);
+				} else {
+					fmt.Printf("Error inserting: %#v\n",err1) }
+			default: 
+				fmt.Printf("Error insertiing: %#v\n",err1) }
+		} else {
+			fmt.Printf("Inserted %#v\n",res)	}
+		}
+	
+	genExpiryDate(db)
+ }
+
+ func openDb(url string) (db *sqlx.DB, err error, closer func()) {
+		db, err = sqlx.Open("postgres", url)
+		closer = func() {
+			err := db.Close()
+			if err != nil {
+				fmt.Printf("Error closing db: %v\n",err)
+				// Not exiting because this function is called from the defer
+			}}
+		return }
+
+	func genExpiryDate(db *sqlx.DB) {
+		res1, err2 := db.Query(`select current_timestamp + interval '1' day`)
+		if err2 != nil {
+			fmt.Printf("Wow!"); os.Exit(1)	}
+		if !res1.Next() {
+			fmt.Printf("No rows here. Why?"); os.Exit(1) }
+		var magic time.Time
+		res1.Scan(&magic)
+		fmt.Printf("Expiry at %s\n", magic.Format("2006-01-02 15:04 -0700"))	}
--- main.go
+++ main.go
@@ -9,53 +9,15 @@ package main
 */
 
 import (
-	"fmt";	"log";	"os"; "time"
-//	neturl "net/url"
-//	"reflect"
-//	"regexp"
-//	"strings"
-//	"time"
-	"github.com/jmoiron/sqlx"
-	"github.com/lib/pq")
+	"fmt" )
 
 func main() {
-	url := "postgresql://localhost:5432"
-
-	db, err, dbCloser := openDb(url)	
-	if err != nil { 
-		log.Printf("Unable to connect to Postgresql, error is %#v", err)
-		os.Exit(1) }
-	defer dbCloser()
-	m := map[string]interface{}{"name" : `",sql 'inject?`}
-	res, err1 := db.NamedExec(`insert into users values (:name)`,
-		m)
-	//xt := reflect.TypeOf(err1).Kind()
-	if err1 != nil {
-		switch e := interface{}(err1).(type)	{
-		case *pq.Error:
-			if e.Code == "23505" {
-				fmt.Printf("Duplicate key in %s",e.Constraint);
-			} else {
-				fmt.Printf("Error inserting: %#v\n",err1) }
-		default: 
-			fmt.Printf("Error insertiing: %#v\n",err1) }
-	} else {
-		fmt.Printf("Inserted %#v\n",res)	}
-	genExpiryDate(db)
-
+	playWithDb()
 	playWithPanic()
 	genNonce(16)
+	playWithHashAndSalt()
 	return }
 
- func openDb(url string) (db *sqlx.DB, err error, closer func()) {
-		db, err = sqlx.Open("postgres", url)
-		closer = func() {
-			err := db.Close()
-			if err != nil {
-				fmt.Printf("Error closing db: %v\n",err)
-				// Not exiting because this function is called from the defer
-			}}
-		return }
 
 	func playWithPanic() {
 		unwind := func() {
@@ -69,12 +31,3 @@ func main() {
 	}
 
 
-	func genExpiryDate(db *sqlx.DB) {
-		res1, err2 := db.Query(`select current_timestamp + interval '1' day`)
-		if err2 != nil {
-			fmt.Printf("Wow!"); os.Exit(1)	}
-		if !res1.Next() {
-			fmt.Printf("No rows here. Why?"); os.Exit(1) }
-		var magic time.Time
-		res1.Scan(&magic)
-		fmt.Printf("Expiry at %s\n", magic.Format("2006-01-02 15:04 -0700"))	}
