d55e0e54b0d6551b8bb73083c61318d114240cf5
Denis Budyak <budden73@gmail.com>
2019-03-06 18:41:40 +0300

    Split main() for future tests

---
+++ application.go
@@ -0,0 +1,44 @@
+package main
+
+// To run this one you need that current user is able to connect
+// to pgsql via postgres://localhost:5432
+// This is achieved like this (unchecked)
+/*
+- run psql via `su postgres`
+- create user budden with superuser login
+- create database budden
+*/
+
+import (
+	"fmt"
+)
+
+const actuallySendEmailP = false
+
+func run(commandLineArgs []string) {
+	playWithDb()
+	playWithPanic()
+	playWithNonce(16)
+	playWithHashAndSalt()
+	/// Uncomment next line to create secret-data.config.json.example
+	//saveSecretConfigDataExample()
+	loadSecretConfigData()
+	if actuallySendEmailP {
+		playWithEmail()
+	} else {
+		fmt.Println("Bypassing sending E-mail due to ActuallySendEmailP == false")
+	}
+	playWithServer()
+	return
+}
+
+func playWithPanic() {
+	unwind := func() {
+		if r := recover(); r != nil {
+			fmt.Printf("recover %#v\n", r)
+			//panic(r)
+		}
+	}
+	defer unwind()
+	panic("It's a panic")
+}
