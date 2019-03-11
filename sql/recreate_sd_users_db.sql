a5357966a55e198eeb72b8664fac0416bc0fbce8
Denis Budyak <budden73@gmail.com>
2019-03-11 19:19:54 +0300

    generate confirmationid, part 2

--- sql/recreate_sd_users_db.sql
+++ sql/recreate_sd_users_db.sql
@@ -31,8 +31,12 @@ CREATE TABLE registrationattempt (
  registrationemail text not null,
  hash text NOT NULL,
  salt text not null,
+ confirmationid text not null,
  registrationtimestamp timestamptz not null default current_timestamp
 );
+
+comment on table registrationattempt is 'registrationattempt gets a new record at each valid registration attempt. We keep registration attemps separated from users table for the case of registration flooding attack';
+comment on column registrationattempt.confirmationid is 'confirmationid is random and as such it can be non-unique. In this case we ask user to re-register';
 
 -- When a user is registrering with a both non-unique nickname and a non-unique E-mail, 
 -- it is unspecified which unique constraint fires first (or I don't know).
@@ -46,6 +50,10 @@ create unique index
 create unique index
  i_registrationattempt__registrationemail
  on registrationattempt(lower(registrationemail));
+
+create unique index
+ i_registrationattempt__confirmationid
+ on registrationattempt(confirmationid);
 
 --- delete_expired_registrationattempts. 
 --- We could run it from the process_registrationformsubmit, but in this case
@@ -66,7 +74,11 @@ $$ language plpgsql;
 
 --- nickname and password must be unique in the union of registrationattempt and sduser tables
 --- use repeatable read transaction and/or single threaded registration processor
-create or replace function process_registrationformsubmit(p_nickname text, p_hash text, p_salt text, p_registrationemail text)
+create or replace function process_registrationformsubmit(p_nickname text
+  ,p_hash text
+  ,p_salt text
+  ,p_registrationemail text
+  ,p_confirmationid text)
 returns void as $$
  BEGIN
   if exists (select 1 from sduser ra where lower(ra.nickname)=lower(p_nickname)) THEN
@@ -75,7 +87,7 @@ returns void as $$
   if exists (select 1 from sduser ra where lower(ra.registrationemail)=lower(p_registrationemail)) THEN
     raise unique_violation using table = 'sduser', column = 'registrationemail', constraint = 'i_sduser_registrationemail';
   end if;
-  insert into registrationattempt(nickname, hash, salt, registrationemail) 
-    values (p_nickname, p_hash, p_salt, p_registrationemail);
+  insert into registrationattempt(nickname, hash, salt, registrationemail, confirmationid) 
+    values (p_nickname, p_hash, p_salt, p_registrationemail, p_confirmationid);
  end;
 $$ language plpgsql;
