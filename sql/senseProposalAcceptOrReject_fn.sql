40fa2c648f9cf1d051524fbe9846877c1dddeb6e
Denis Budyak <budden73@gmail.com>
2019-06-23 16:06:18 +0300

    progress on proposal accept/reject (does not work now)

--- sql/senseProposalAcceptOrReject_fn.sql
+++ sql/senseProposalAcceptOrReject_fn.sql
@@ -50,20 +50,56 @@ $$;
 
   Returns common id, or if it was a deletionproposed, 
   special value of -1
+
+  FIXME защищаться от изменения записи другим пользователем во время
+  просмотра.
  */
 -- Продолжать реализацию слияния смыслов
 --  сделать удаление и добавление смысла. Сразу историю?
 create or replace function fnacceptorrejectsenseproposal(
-    p_proposalid bigint, p_acceptorreject bigint)
+    p_sduserid bigint, p_proposalid bigint, p_acceptorreject bigint)
   returns table (r_commonid bigint)
   language plpgsql as $$
-  declare v_phantom bool;
+  declare v_common_phantom bool;
+  declare v_deletionproposed bool;
+  declare v_languageid int;
+  declare v_common_languageid int;
+  declare v_row_count int;
   declare update_count int;
   declare v_commonid bigint;
   declare v_proposalid bigint;
+  declare v_have_privilege bool;
   begin
   p_proposalid = coalesce(p_proposalid,0);
-  p_commonid = coalesce(p_commonid,0);
+  lock table themutex;
+  select languageid, phantom, deletionproposed, originid 
+    from tsense where id=p_proposalid 
+    into v_languageid, v_deletionproposed, v_commonid;
+  get diagnostics v_row_count = row_count;
+  if v_row_count != 1 then
+    raise exception 'invalid p_proposalid'; end if;
+  -- Check correctness and privileges
+  if v_commonid is not null then
+    select languageid, phantom from tsense 
+      where id=v_commonid 
+      into v_common_languageid, v_common_phantom;
+      get diagnostics v_row_count = row_count;
+      if v_row_count != 1 then
+        raise exception 'invalid proposal (common sense is missing)'; end if; 
+      if coalesce(v_languageid,0) <> coalesce(v_common_languageid,0) then
+        raise exception 'invalid proposal (language mismatch)'; end if; end if;
+  select result from isuserhavelanguageprivilege(p_sduserit
+    ,4/*'Accept/decline change requests'*/, v_languageid) into v_have_privilege;
+  if not v_have_privilege then
+    raise exception 'sorry, you have no right to act on this proposal'; end if;
+  -- если отказ, то поменять статус и выйти.
+  -- если уже удалено и хотим удалить, то отказываем
+  -- если уже удалено и хотим поменять, то восстанавливаем
+  -- если добавление, то добавляем
+  -- если правка, то правим. 
+  return query(select cast(0 as bigint));
+
+/*  p_commonid = coalesce(p_commonid,0);
   if coalesce(p_proposalstatus,'n/a') = 'n/a' then
     raise exception 'proposal status must be not null, not "n/a"'; end if;
   if p_evenifidentical then
@@ -95,7 +131,7 @@ create or replace function fnacceptorrejectsenseproposal(
   get diagnostics update_count = row_count;
   if update_count != 1 then
     raise exception 'expected to update just one record, which didn''t hapen'; end if;
-  return query(select v_proposalid); return; end;
+  return query(select v_proposalid); */ return; end; 
 $$;
 
 \echo *** senseProposalAcceptOrReject_fn.sql Done
