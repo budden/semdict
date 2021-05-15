0d1b70febf753fe9e06d36bf09cc05a294fcc020
Сандро <warsan00@mail.ru>
2021-05-16 03:18:13 +0500

    перевод комментов

--- static/select2-4.0.7/src/js/banner.start.js
+++ static/select2-4.0.7/src/js/banner.start.js
@@ -1,6 +1,6 @@
 (function () {
-  // Restore the Select2 AMD loader so it can be used
-  // Needed mostly in the language files, where the loader is not inserted
+  // Восстановите загрузчик Select2 AMD, чтобы его можно было использовать
+  // Нужен в основном в языковых файлах, куда загрузчик не вставляется
   if (jQuery && jQuery.fn && jQuery.fn.select2 && jQuery.fn.select2.amd) {
     var S2 = jQuery.fn.select2.amd;
   }
