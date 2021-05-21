afcc30573394dc3bbebd53282740b0d6a9c27707
Сандро <warsan00@mail.ru>
2021-05-22 00:11:57 +0500

    перевод

--- pkg/query/senseEditSubmit.go
+++ pkg/query/senseEditSubmit.go
@@ -17,7 +17,7 @@ import (
 
 type senseEditSubmitDataType struct {
 	Sduserid int64
-	Senseid  int64 // must be here
+	Senseid  int64 // должен быть здесь
 	OWord    string
 	Theme    string
 	Phrase   string
@@ -25,7 +25,7 @@ type senseEditSubmitDataType struct {
 	Action   string
 }
 
-// SenseEditSubmitPostHandler posts an sense data
+// SenseEditSubmitPostHandler размещает данные о смысле
 func SenseEditSubmitPostHandler(c *gin.Context) {
 	user.EnsureLoggedIn(c)
 	pad := &senseEditSubmitDataType{}
@@ -41,19 +41,19 @@ func SenseEditSubmitPostHandler(c *gin.Context) {
 			"/sensedeleteconfirm/"+strconv.FormatInt(pad.Senseid, 10))
 		// spdp := &senseDeleteParamsType{Sduserid: pad.Sduserid, Senseid: pad.Senseid}
 	} else {
-		apperror.Panic500AndErrorIf(apperror.ErrDummy, "Unknown action in the form")
+		apperror.Panic500AndErrorIf(apperror.ErrDummy, "Неизвестное действие в форме")
 	}
 
 }
 
 func sanitizeData(pad *senseEditSubmitDataType) {
-	// example just from the title page of https://github.com/microcosm-cc/bluemonday
+	// пример только с титульного листа https://github.com/microcosm-cc/bluemonday
 	p := bluemonday.UGCPolicy()
 	pad.Phrase = p.Sanitize(pad.Phrase)
 	matched, err := regexp.Match(`^[0-9a-zA-Z\p{L} ]+$`, []byte(pad.OWord))
 	if (err != nil) || !matched {
 		// https://www.linux.org.ru/forum/development/14877320
-		apperror.Panic500AndErrorIf(apperror.ErrDummy, "English word can only contain latin letters, digits and spaces")
+		apperror.Panic500AndErrorIf(apperror.ErrDummy, "Английское слово может содержать только латинские буквы, цифры и пробелы")
 	}
 }
 
@@ -70,7 +70,7 @@ func extractDataFromRequest(c *gin.Context, pad *senseEditSubmitDataType) {
 func writeToDb(pad *senseEditSubmitDataType) (newProposalid int64) {
 	res, err1 := sddb.NamedUpdateQuery(
 		`select fnsavesense(:sduserid, :senseid, :oword, :theme, :phrase, :ownerid)`, pad)
-	apperror.Panic500AndErrorIf(err1, "Failed to update a sense")
+	apperror.Panic500AndErrorIf(err1, "Не удалось обновить смысл")
 	defer sddb.CloseRows(res)()
 	dataFound := false
 	for res.Next() {
@@ -78,7 +78,7 @@ func writeToDb(pad *senseEditSubmitDataType) (newProposalid int64) {
 		dataFound = true
 	}
 	if !dataFound {
-		apperror.Panic500AndErrorIf(apperror.ErrDummy, "No proposal id from server")
+		apperror.Panic500AndErrorIf(apperror.ErrDummy, "Нет идентификатора предложения с сервера")
 	}
 	return
 }
