a11f4310ea54497ee6dc4473e959f907dc76026c
Сандро <warsan00@mail.ru>
2021-05-22 01:24:59 +0500

    перевод

--- pkg/shared/secretconfigdata.go
+++ pkg/shared/secretconfigdata.go
@@ -1,11 +1,11 @@
 package shared
 
-// SecretConfigDataT specifies the fields of semdict.config.json
-// That file contains the data which is secret and site-specific so it can't be stored to git
+// SecretConfigDataT определяет поля semdict.config.json
+// Этот файл содержит данные, которые являются секретными и специфичными для конкретного сайта, поэтому они не могут быть сохранены в git
 type SecretConfigDataT struct {
 	Comment             []string
 	SiteRoot            string
-	UnderAProxy         int8 // 0 means false, 1 means true
+	UnderAProxy         int8 // 0 означает ложь, 1 - истину
 	ServerPort          string
 	SMTPServer          string
 	SMTPUser            string
@@ -14,29 +14,29 @@ type SecretConfigDataT struct {
 	PostgresqlServerURL string
 	TLSCertFile         string
 	TLSKeyFile          string
-	// If set to non-zero, acts as if a user with this id is always logged in,
-	// useful for debugging of user-based routes
+	// Если установлено ненулевое значение, действует так, будто пользователь с этим идентификатором всегда входит в систему,
+	// что полезно для отладки маршрутов, основанных на пользователях.
 	UserAlwaysLoggedIn int
-	// Some gin messages are annoying, set this switch to 1 to hush them
+	// Некоторые сообщения gin раздражают, установите этот переключатель на 1, чтобы заглушить их.
 	HideGinStartupDebugMessages int
-	// Set GinDebugMode to 1 to enable gin debug mode
+	// Установите GinDebugMode в 1, чтобы включить режим отладки gin
 	GinDebugMode int
 }
 
-// SecretConfigDataTComment is actually a documentation for SecretConfigData, which is placed to a config sample file
-var SecretConfigDataTComment = []string{"Example config file. Copy this one to the semdict.config.json and edit.",
-	"UnderAProxy is an integer value with legal values of 0 (false) and 1 (true)",
-	"Set UnderAProxy to 0 if gin is used as a web server (standalone mode)",
-	"UnderAProxy to 1 when semdict is started as a service behind a TLS-enabled reverse proxy (service mode)",
-	"ServerPort is included into the registration E-mails only is UnderAProxy == 1.",
-	"TLSCertFile and TLSKeyFile (PEM format) can only be used in a standalone mode to enable https",
-	"Pass empty strings to use plain http",
-	"If an SMTPServer is set to an empty string, emails are printed to stdout instead of actually being sent"}
+// SecretConfigDataTComment - это фактически документация для SecretConfigData, которая помещается в файл образца конфигурации
+var SecretConfigDataTComment = []string{"Пример конфигурационного файла. Скопируйте его в файл semdict.config.json и отредактируйте.",
+	"UnderAProxy - целочисленное значение с допустимыми значениями 0 (false) и 1 (true)",
+	"Установите UnderAProxy на 0, если gin используется в качестве веб-сервера (автономный режим)",
+	"UnderAProxy - 1, когда semdict запускается как служба за обратным прокси-сервером с поддержкой TLS (режим службы).",
+	"ServerPort включается в регистрацию E-mails только в том случае, если UnderAProxy == 1.",
+	"TLSCertFile и TLSKeyFile (формат PEM) можно использовать только в автономном режиме для включения https",
+	"Передавайте пустые строки для использования обычного http",
+	"Если SMTPServer установлен на пустую строку, электронные письма выводятся в stdout, а не отправляются."}
 
-// SecretConfigData is an in-memory copy of a semdict.config.json configuration file
+// SecretConfigData - это копия в памяти конфигурационного файла semdict.config.json
 var SecretConfigData *SecretConfigDataT
 
-// SitesProtocol returns "http:" or "https:"
+// SitesProtocol возвращает "http:". или "https:".
 func SitesProtocol() string {
 	scd := SecretConfigData
 	if scd.UnderAProxy == 1 || scd.TLSKeyFile != "" {
@@ -45,8 +45,8 @@ func SitesProtocol() string {
 	return "http:"
 }
 
-// SitesPort returns "port:" if there is a non-standard port.
-// Under a proxy, returns nothing
+// SitesPort возвращает "порт:". если имеется нестандартный порт.
+// По доверенности, ничего не возвращает
 func SitesPort() string {
 	scd := SecretConfigData
 	if scd.UnderAProxy == 1 {
